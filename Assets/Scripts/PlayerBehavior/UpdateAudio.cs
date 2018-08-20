e51c281bcc055ce7b02549a5a4f100a7ec3a9875
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-19 22:14:04 -0600

    Player Debug
    Added basic HUD canvas. Added player jump sound. Fixed a bug where player's foot hitbox would catch on the floor after some collisions.

---
+++ Assets/Scripts/PlayerBehavior/UpdateAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAudio : MonoBehaviour {
+
+	private bool jumpSoundPlayed;
+	
+	private PlayerState state;
+	private AudioSource jump;
+
+	// Use this for initialization
+	void Awake () {
+		jump = GetComponent<AudioSource>();
+		state = GetComponent<PlayerState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!state.onGround && !jumpSoundPlayed){
+			jump.Play();
+			jumpSoundPlayed = true;
+		}
+		
+		if(state.onGround){
+			jumpSoundPlayed = false;
+		}
+	}
+}
