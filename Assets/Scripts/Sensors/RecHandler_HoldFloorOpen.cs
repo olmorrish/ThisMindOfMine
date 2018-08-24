b5f00ab5fcd3ebfbcf0c05b84ebf30185f27fe0c
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-23 23:59:24 -0600

    Openable Floor Added
    Added openable floor tile. Added more early level design.

---
+++ Assets/Scripts/Sensors/RecHandler_HoldFloorOpen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecHandler_HoldFloorOpen : MonoBehaviour {
+
+	private Reciever rec;
+	private Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+		rec = gameObject.GetComponent<Reciever>();
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(rec.pulse == true){
+			//TODO: finish, only animation
+			animator.SetBool("isLocked", false);
+			GetComponent<Collider2D>().enabled = false;
+		}
+		
+		else{
+			animator.SetBool("isLocked", true);
+			GetComponent<Collider2D>().enabled = true;
+		}
+		
+	}
+}
