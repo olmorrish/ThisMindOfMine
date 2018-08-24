dadae5af4cb454e6256569972391016efc39fe60
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-24 05:45:56 -0600

    Added Button Prompts
    Added button prompt system with corresponding controller animations. Polished puzzles in tutorial area, Zone 1. Began layout/planning for Zone 2.

---
+++ Assets/Scripts/DisplayPrompt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayPrompt : MonoBehaviour {
+
+	private Renderer rend;
+	private Renderer[] renderers;
+	private Collider2D parentTipStoneHB;
+	private Collider2D playerHB;
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<Renderer>();
+		renderers = GetComponentsInChildren<Renderer>();
+		parentTipStoneHB = gameObject.transform.parent.gameObject.GetComponent<Collider2D>();
+		playerHB = GameObject.Find("Player").GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(parentTipStoneHB.IsTouching(playerHB)){
+			foreach(Renderer rend in renderers) {
+				rend.enabled = true;
+
+			}
+		}
+		else{
+			foreach(Renderer rend in renderers) {
+				rend.enabled = false;
+
+			}
+		}
+		
+	}
+}
