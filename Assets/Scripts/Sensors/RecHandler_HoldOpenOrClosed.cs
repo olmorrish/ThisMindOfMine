5798865088b562526e71aaa9bf28e77c9da69774
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-25 00:19:13 -0600

    Object Bugfixes
    Fixed an issue where floor and door gate objects were using identical-function scripts in differently-named files. Prefabs for both have been updated with a shared pulse reciever handling script.

---
+++ Assets/Scripts/Sensors/RecHandler_HoldOpenOrClosed.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecHandler_HoldOpenOrClosed : MonoBehaviour {
+
+	private Reciever rec;
+	private Animator animator;
+	public bool invertBehavior = false;
+
+	// Use this for initialization
+	void Awake () {
+		rec = gameObject.GetComponent<Reciever>();
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(!invertBehavior){
+			if(rec.pulse == true){
+				//TODO: finish, only animation
+				animator.SetBool("isLocked", false);
+				GetComponent<Collider2D>().enabled = false;
+			}
+			
+			else{
+				animator.SetBool("isLocked", true);
+				GetComponent<Collider2D>().enabled = true;
+			}
+		}
+		
+		else{
+			if(rec.pulse == false){
+				//TODO: finish, only animation
+				animator.SetBool("isLocked", false);
+				GetComponent<Collider2D>().enabled = false;
+			}
+			
+			else{
+				animator.SetBool("isLocked", true);
+				GetComponent<Collider2D>().enabled = true;
+			}
+
+		}
+		
+		
+		
+	}
+}
