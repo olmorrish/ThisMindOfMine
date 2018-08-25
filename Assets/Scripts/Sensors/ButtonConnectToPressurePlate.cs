291bc90a8a2194b32e9df8f920e126b82b0dd986
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-24 18:23:18 -0600

    Button Overhaul
    Replaced button sprites. Fixed a bug where on multiple items pressing them, buttons would decativate if one item left. Buttons will now function with a pring joint pressure-plate as opposed to collision triggers.

---
+++ Assets/Scripts/Sensors/ButtonConnectToPressurePlate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonConnectToPressurePlate : MonoBehaviour {
+
+	public Rigidbody2D plateReference;
+	public SpringJoint2D spring; 
+
+	// Use this for initialization
+	void Start () {
+		spring = GetComponent<SpringJoint2D>();
+		
+		
+		plateReference = gameObject.transform.Find("PressurePlate").GetComponent<Rigidbody2D>();
+		spring.connectedBody = plateReference;
+
+		
+		
+		
+	}
+}
