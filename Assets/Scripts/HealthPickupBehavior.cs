604d4530184c9c512e3f496dd0749aa31712bf5e
Oliver Morrish <olmorris@ucalgary.ca>
2018-08-26 21:07:39 -0600

    Added Health Pickup
    Added health pickup that respawns on a 10 second cooldown and refills player HP.

---
+++ Assets/Scripts/HealthPickupBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickupBehavior : MonoBehaviour {
+
+	private Collider2D player;
+	private Collider2D hitbox;
+	private Animator animator;
+	private Animator backdropAnimator; 
+	
+	public int cooldownMax = 900;
+	public int cooldown;
+	bool onCD = false;
+	
+	private GameStateFlags gameState;
+	private PlayerState playerState;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player").GetComponent<Collider2D>();
+		hitbox = GetComponent<Collider2D>();
+		animator = GetComponent<Animator>();
+		backdropAnimator = gameObject.transform.Find("TransparentBackdrop").GetComponent<Animator>();
+		
+		cooldown = 0;
+		
+		gameState = GameObject.Find("GameState").GetComponent<GameStateFlags>();
+		playerState = GameObject.Find("Player").GetComponent<PlayerState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(cooldown>0){
+			cooldown -= 1;
+		}
+		
+		else if(hitbox.IsTouching(player)){
+			playerState.health = 3;
+			hitbox.enabled = false;
+			animator.SetBool("collected", true);
+			backdropAnimator.SetBool("collected", true);
+			
+			cooldown = cooldownMax;
+		}
+		
+		else{
+			hitbox.enabled = true;
+			animator.SetBool("collected", false);
+			backdropAnimator.SetBool("collected", false);
+		}
+		
+		
+	}
+}
